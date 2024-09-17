1fe49fdab5828c868badd1179af9bc740555fb3e
cbogart91 <166065354+cbogart91@users.noreply.github.com>
2024-09-17 18:56:07 -0500

    added mongodb and other server stuff

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
-
+const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleWare } = require('@apollo/server/express4');
 
 const { typeDefs, resolvers }= require('./schemas');
 const db = require('./config/connection');
+
+const port = process.env.PORT || 3000;
+const path = require('path');
+const app = express();
 
 
 const server = new ApolloServer({
@@ -13,34 +17,11 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
     await server.start();
-
-
-    if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/dist')));
-        
-        app.get('*', (req, res) => {
-          res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-        });
-}
-
-app.use('/graphql', expressMiddleWare(server));
-
-db.once('open', () => {
-    app.listen(PORT, () =>{
-        console.log(`API server running on port ${PORT}`);
-        console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
-        });
-    });
-};
-
-startApolloServer();const express = require('express');
-const path = require('path');
-const app = express();
-const port = process.env.PORT || 3000;
-
 // Middleware to parse JSON bodies
-app.use(express.urlencoded({ extended: false }));
-  app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    app.use('/graphql', expressMiddleWare(server));
 
 // Define a route 
 app.get('/', (req,res) => {
@@ -48,6 +29,15 @@ app.get('/', (req,res) => {
 });
 
 // Start the server
-app.listen(port, () => {
-    console.log('Server is running at http://localhost:3000');
-});
+db.once('open', () => {
+    app.listen(port, () => {
+        console.log('Server is running at http://localhost:3000');
+        console.log('Use GraphQL at http://localhost:${PORT}/graphql);
+        });
+    });
+};
+
+startApolloServer();
+
+
+
