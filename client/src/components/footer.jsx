5079726bb2074ed1ca2f601aaf4992c968ef620e
bl00dy hazak <kennethhass1014@gmail.com>
2024-09-18 11:04:30 -0400

    added the footer

---
+++ client/src/components/footer.jsx
@@ -0,0 +1,31 @@
+import './Footer.css';
+
+function Footer () {
+
+    return(
+        
+        <div id='footer'>
+            <div className='container'>
+                <footer id='connect' className='py-3'>
+                    <h4>Our Contacts</h4>
+                    <h2>Malik Kerek</h2>
+                    <a href="https://github.com/malik061103">GitHub</a>
+
+                    <h2>David Ayala-Irizarry</h2>
+                    <a href="https://github.com/elborisgpr">GitHub</a>
+
+                    <h2>Cody Bogart</h2>
+                    <a href="https://github.com/cbogart91">GitHub</a>
+
+                    <h2>Nino Gorgadze</h2>
+                    <a href="https://github.com/zviangi10">GitHub</a>
+                    
+                    <h2>Kenneth Hasson</h2>
+                    <a href="https://github.com/kenneth-hass">GitHub</a>
+                </footer>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
