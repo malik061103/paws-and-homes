d72b96285a7abcb9f78d720bb1b6af5fc294c049
malik061103 <malikkerek08@gmail.com>
2024-09-17 21:24:17 -0400

    finished models

--- server/models/Application.js
+++ server/models/Application.js
@@ -2,25 +2,37 @@ const { Schema } = reqiured('mongoose');
 
 const applicantionSchema = new Schema(
     {
-    firstName:{
-        type:String,
-        reqiured:true,
-        
-    },
-    lastName:{
-        type:String,
-        reqiured:true
-    },
-    phoneNummber:{
-        type:Number,
-        reqiured:true
-    },
-    email:{
-        type:String,
-        reqiured:true,
-        unique:true
-    },
+        firstName: {
+            type: String,
+            required: true,
 
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        phoneNummber: {
+            type: Number,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        description: {
+            type: String,
+            minimum: 0,
+            maximum: 100,
+        },
+        applicantionStatus: {
+            type: String,
+            required: true
+        },
+        pet: {
+            type: String,
+
+        }
     }
 )
 
