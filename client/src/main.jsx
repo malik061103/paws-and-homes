41bd0b8b36bc334a31e4fc50b69544da007fba48
sgprboricua <sgprboricua@hotmail.com>
2024-09-25 20:10:39 -0400

    m

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,37 +1,10 @@
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Cat from './components/CatPage.jsx';
-import Dog from './components/DogPage.jsx';
-import Home from './pages/HomePage.jsx';
-import Adoption from './pages/AdoptionPage.jsx';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "/dog",
-        element: <Dog />
-      },
-      {
-        path: "/cat",
-        element: <Cat />
-      },
-      {
-        path: "/adoption",
-        element: <Adoption />
-      },
-    ]
-  }
-]);
-
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
